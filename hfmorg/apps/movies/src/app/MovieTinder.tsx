2ff2ecc5e2b58abad4370bd82b32cbcccbdc75bd
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-10-21 14:49:27 +0200

    basic template ready

---
+++ hfmorg/apps/movies/src/app/MovieTinder.tsx
@@ -0,0 +1,74 @@
+/** @jsxImportSource theme-ui */
+import { Button, Flex, Heading, Paragraph } from 'theme-ui'
+
+import { movies } from '../fake-api/index'
+
+import TinderCard from 'react-tinder-card'
+
+import { useState } from 'react';
+
+export function MovieTinder() {
+  const moviesAPI: any = movies;
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction: any, nameToDelete: any) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name: any) => {
+    console.log(name + ' left the screen!')
+  }
+
+  
+  return (
+   
+      <Flex sx={{justifyContent: 'center',
+       alignItems: 'center',
+        minHeight: "100vh",
+         width: '100vw',
+          textAlign: 'center',
+           overflow: 'hidden',
+            flexDirection: 'column',
+            backgroundColor: 'green'
+            }}>
+      {moviesAPI.map((movie: any) =>
+      <Flex sx={{
+        width: "90vw", maxWidth: "260px", height: "300px"
+        }}>
+        <TinderCard sx={{position: 'absolute', top: '10vh' }} key={movie.title} onSwipe={(dir) => swiped(dir, movie.title)} onCardLeftScreen={() => outOfFrame(movie.title)}>
+                    <Flex sx={{position: "relative",
+                      width: "90vw", maxWidth: "260px", height: "300px",
+                         backgroundImage: 'url(' + movie.imageURL + ')',
+                          backgroundSize: 'cover',backgroundPosition: "center"
+                        }}>
+                      
+                                         
+                    </Flex>
+                      <Flex sx={{flexDirection: 'column', maxWidth: '260px', background: 'black', color: 'white'}}>
+                            <Paragraph sx={{padding: 10,
+                                 fontWeight: 'bold'}} >{movie.title}</Paragraph>
+                            <Paragraph sx={{paddingBottom: 10,
+                                 minHeight: '40px',
+                                 maxHeight: '100px',
+                                  overflowY: 'scroll'}} >{movie.summary}</Paragraph>
+                            <Heading sx={{position: 'absolute', top: 0, padding: 10, fontSize: 33}}>{`${movie.rating}/10`}</Heading>
+                            <Flex sx={{                            
+                              width: '100%',
+                               justifyContent: 'space-around',
+                               padding: '10px',
+                               marginBottom: '20px' }}>
+                                <Button>accept</Button>
+                                <Button>reject</Button>
+                            </Flex>  
+                      </Flex>
+        </TinderCard>
+      </Flex>
+         
+        )}
+      </Flex>    
+  
+  )
+}
+export default MovieTinder;
+
--- hfmorg/apps/movies/src/app/app.tsx
+++ hfmorg/apps/movies/src/app/app.tsx
@@ -7,58 +7,18 @@ import { movies } from '../fake-api/index'
 import TinderCard from 'react-tinder-card'
 
 import { useState } from 'react';
+import MovieTinder from './MovieTinder';
 
 export function App() {
-  const moviesAPI: any = movies;
-  const [lastDirection, setLastDirection] = useState();
-
-  const swiped = (direction: any, nameToDelete: any) => {
-    console.log('removing: ' + nameToDelete)
-    setLastDirection(direction)
-  }
-
-  const outOfFrame = (name: any) => {
-    console.log(name + ' left the screen!')
-  }
 
 
 
-  // const onSwipe = (direction: any) => {
-  //   console.log('You swiped: ' + direction)
-  // }
-  
-  // const onCardLeftScreen = (myIdentifier: any) => {
-  //   console.log(myIdentifier + ' left the screen')
-  // }
+
+
   
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Flex sx={{justifyContent: 'center',
-       alignItems: 'center',
-        minHeight: "100vh",
-         width: '100vw',
-          textAlign: 'center',
-           overflow: 'hidden',
-            flexDirection: 'column'
-            }}>
-      {moviesAPI.map((movie: any) =>
-      <Flex sx={{
-        justifyContent: 'center'
-        }}>
-        <TinderCard key={movie.title} onSwipe={(dir) => swiped(dir, movie.title)} onCardLeftScreen={() => outOfFrame(movie.title)}>
-                    <Box sx={{
-                       height: "80vh",
-                        width: "80vw",
-                         backgroundImage: 'url(' + movie.imageURL + ')',
-                          backgroundRepeat: 'no-repeat'
-                        }}>
-                      <Heading>{movie.title}</Heading>
-                    </Box>
-        </TinderCard>
-      </Flex>
-         
-        )}
-      </Flex>    
+      <MovieTinder /> 
     </ThemeProvider>
   )
 }
