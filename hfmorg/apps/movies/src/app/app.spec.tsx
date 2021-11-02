123f5ce066eab289c2829a8ef41af3e8ae0d774f
wojtek0126 <wojtek0126@gmail.com>
2021-11-02 12:51:29 +0100

    cleaner test file, writing test cases in jest jest practice

--- hfmorg/apps/movies/src/app/app.spec.tsx
+++ hfmorg/apps/movies/src/app/app.spec.tsx
@@ -1,11 +1,9 @@
-import { render, screen, waitFor, cleanup, act } from '@testing-library/react';
+import { render, screen, cleanup, act } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
 import App from './app';
-import MovieTinder from './MovieTinder';
-import TinderCard from 'react-tinder-card';
-
-
+// import MovieTinder from './MovieTinder';
+// import TinderCard from 'react-tinder-card';
 
 const  globalRef:any = global;
 
@@ -24,26 +22,6 @@ afterEach(() => {
   cleanup();
 })
 
-
-
-
-// describe('App', () => {
-//   it('should render successfully', () => {
-   
-//     const { baseElement, debug } = render(<MovieTinder />);
-//     console.log(debug());
-//     expect(baseElement).toBeTruthy();
-//   });
-
-//   it('should have a title', () => {
-//     const { getByText } = render(<App />);
-
-//     expect(getByText("ChozzAndWatch")).toBeInTheDocument();
-//   });
-
-
-
-
 describe("App", () => {
     it('should render successfully', () => {   
     const { baseElement, debug } = render(<App />);
@@ -51,28 +29,35 @@ describe("App", () => {
     expect(baseElement).toBeTruthy();
   });
 
-
+  it('should display title in header', () => {   
+    const { getByText, debug } = render(<App />);
+    console.log(debug());
+    expect(getByText("ChozzAndWatch")).toBeInTheDocument();
+  });
 
   it("should load data on mount", async () => {
     //@ts-ignore
-    await act(async () => render(<MovieTinder />))
+    await act(async () => render(<App />))
     expect(screen.getByText("Inferno(5.3/10)")).toBeInTheDocument();
   })
+
+  it("should load card with button Accept", async () => {
+    //@ts-ignore
+    await act(async () => render(<App />))
+    expect(screen.getByText("Accept")).toBeInTheDocument();
+  })
+
+  it("should load card with button Decline", async () => {
+    //@ts-ignore
+    await act(async () => render(<App />))
+    expect(screen.getByText("Decline")).toBeInTheDocument();
+  })
+
+
 })
  
     
-    // await act(async () => debug(<App />));
-    
-      // await waitFor(() => {
-      // })
-    // expect(screen.getByText("ChozzAndWatch")).toBeInTheDocument(); 
-//   });
-// });
 
-// describe('matches snapshot', () => {
-//   const tree = renderer.create(<MovieTinder />).toJSON;
-//   console.log(tree)
-// })
 
 
 
