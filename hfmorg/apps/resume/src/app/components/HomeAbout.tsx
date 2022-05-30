4f1ece4ea90619338791457bc137c22f688eef4d
LionbridgeOffice <jakub.golebiewski@lionbridge.com>
2022-05-30 09:46:25 +0200

    add about section and add about english content

--- hfmorg/apps/resume/src/app/components/HomeAbout.tsx
+++ hfmorg/apps/resume/src/app/components/HomeAbout.tsx
@@ -1,5 +1,15 @@
-import { Flex, Paragraph } from "theme-ui";
-import { aboutMeText } from '../content/contentEN';
+import { Flex, Heading, Paragraph } from "theme-ui";
+import {
+    aboutMeText,
+    aboutGoalsHeading,
+    aboutGoalsText,
+    aboutSkillsHeading,
+    aboutSkillsText,
+    aboutAbilitiesHeading,
+    aboutAbilitiesText,
+    aboutInterestsHeading,
+    aboutInterestsText
+} from '../content/contentEN';
 import about from 'C:/Users/GPC-393/Desktop/HFM_workspace/hfmorg/apps/resume/src/assets/about.jpg';
 
 
@@ -14,7 +24,7 @@ export function HomeAbout() {
                 }}>  
                  <img src={about} alt="about me" style={{maxWidth: '100%'}}/>  
                  <Flex sx={{
-                            margin: '80px 20px',
+                            margin: '20px ',
                             backgroundColor: 'colorBackgroundTextAbout',
                             boxShadow: '5px 10px #888888',
                             padding: '20px', 
@@ -22,6 +32,58 @@ export function HomeAbout() {
                             justifyContent: 'center', 
                             alignItems: 'center'}}>
                         {aboutMeText}
+                </Flex>
+                <Flex sx={{flexDirection: 'row',
+                           justifyContent: 'center', 
+                           alignItems: 'center'}}>
+                <Flex sx={{
+                            margin: '20px',
+                            backgroundColor: 'colorBackgroundTextAbout',
+                            boxShadow: '5px 10px #888888',
+                            padding: '20px', 
+                            textAlign: 'center', 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            flexDirection: 'column'}}>
+                                <Heading>{aboutGoalsHeading}</Heading>
+                                <Paragraph>{aboutGoalsText}</Paragraph>
+                            </Flex>
+                    <Flex sx={{
+                            margin: '20px',
+                            backgroundColor: 'colorBackgroundTextAbout',
+                            boxShadow: '5px 10px #888888',
+                            padding: '20px', 
+                            textAlign: 'center', 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            flexDirection: 'column'}}>
+                                <Heading>{aboutSkillsHeading}</Heading>
+                                <Paragraph>{aboutSkillsText}</Paragraph>
+                            </Flex>
+                    <Flex sx={{
+                            margin: '20px',
+                            backgroundColor: 'colorBackgroundTextAbout',
+                            boxShadow: '5px 10px #888888',
+                            padding: '20px', 
+                            textAlign: 'center', 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            flexDirection: 'column'}}>
+                                 <Heading>{aboutAbilitiesHeading}</Heading>
+                                <Paragraph>{aboutAbilitiesText}</Paragraph>
+                            </Flex>
+                    <Flex sx={{
+                            margin: '20px',
+                            backgroundColor: 'colorBackgroundTextAbout',
+                            boxShadow: '5px 10px #888888',
+                            padding: '20px', 
+                            textAlign: 'center', 
+                            justifyContent: 'center', 
+                            alignItems: 'center',
+                            flexDirection: 'column'}}>
+                                 <Heading>{aboutInterestsHeading}</Heading>
+                                <Paragraph>{aboutInterestsText}</Paragraph>
+                            </Flex>
                 </Flex>
         </Flex>     
     </Flex>
--- hfmorg/apps/resume/src/app/content/contentEN.js
+++ hfmorg/apps/resume/src/app/content/contentEN.js
@@ -1,9 +1,29 @@
+export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navAbout = "about me";
+// export const navTech = "my tech";
+// export const navinterests = "my interests";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+// export const navShowbox = "showbox";
+
 
 export const carouselText1 = "Kanban tasks, Logging defects, retesting, regression with JIRA";
 export const carouselText2 = "Creating test scenarios and test cases, executing test runs using Testrail";
 export const carouselText3 = "Product and test documentation with Confluence";
 export const carouselText4 = "Cooperating with Azure Devops";
 
-
-export const aboutMeHeader = "About Me"
+export const aboutMeHeader = "About Me";
 export const aboutMeText = "Hi. My name is Wojtek. I am a tester with programming skills. ";
+export const aboutGoalsHeading = "My Goals"; 
+export const aboutGoalsText = "My next step IT goal is to incorporate automation to my current professional skillset.";
+export const aboutSkillsHeading = "My Skills"; 
+export const aboutSkillsText = "My professionally proven skills are testing and quality assurance. In 2019 I was QA Engineer at DELL EMC in cork, Ireland. Currently I work as Test Engineer in Warsaw. I actively practice JavaScript since 2020, particularily with React.js. Beside this, I also have some non-commercial experience using Python and C#. I have basic SQL quering knowledge"
+export const aboutAbilitiesHeading = "My Abilities"; 
+export const aboutAbilitiesText = "I am creative problem solver and I love constant improvement. My most valuable ability is empathy that I use to keep happy and motivated circle around. I can keep positive attitude and make right moves to have the job done";
+export const aboutInterestsHeading = "My Interests"; 
+export const aboutInterestsText = "I am interested in sports, technology, personal development and all kinds of arts. I am training and competing in various martial arts competitions. I like challenge and victory."
